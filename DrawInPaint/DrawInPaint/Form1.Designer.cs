f4cfb0061c3cfab7882430d0bd71366b6109e455
atbash111 <chuongtangkhanh2104@gmail.com>
2019-09-18 09:15:28 +0700

    Hello

---
+++ DrawInPaint/DrawInPaint/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DrawInPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.ButtonPanel = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.RectangleButton = new System.Windows.Forms.Button();
+            this.ButtonPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ColorButton.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.ColorButton.Location = new System.Drawing.Point(781, 3);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(77, 73);
+            this.ColorButton.TabIndex = 0;
+            this.ColorButton.UseVisualStyleBackColor = false;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // ButtonPanel
+            // 
+            this.ButtonPanel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ButtonPanel.Controls.Add(this.comboBox1);
+            this.ButtonPanel.Controls.Add(this.RectangleButton);
+            this.ButtonPanel.Controls.Add(this.ColorButton);
+            this.ButtonPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ButtonPanel.Location = new System.Drawing.Point(0, 0);
+            this.ButtonPanel.Name = "ButtonPanel";
+            this.ButtonPanel.Size = new System.Drawing.Size(998, 78);
+            this.ButtonPanel.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(506, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // RectangleButton
+            // 
+            this.RectangleButton.Location = new System.Drawing.Point(678, 12);
+            this.RectangleButton.Name = "RectangleButton";
+            this.RectangleButton.Size = new System.Drawing.Size(78, 63);
+            this.RectangleButton.TabIndex = 1;
+            this.RectangleButton.Text = "Rectangle";
+            this.RectangleButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(998, 588);
+            this.Controls.Add(this.ButtonPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ButtonPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.Panel ButtonPanel;
+        private System.Windows.Forms.Button RectangleButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ DrawInPaint/DrawInPaint/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawInPaint
+{
+    public partial class Form1 : Form
+    {
+        //Khai bao bien
+        Point old = new Point();
+        Point current = new Point();
+        Color currentColor = Color.Black;
+        Pen pen;
+        Graphics gra;
+        Bitmap bm;
+        enum Tool{PEN, BRUSH, ERASER, SPECIAL_SHAPE };
+
+        int currentSize = 1;
+        bool isDown = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitComboBox();
+            bm = new Bitmap(this.Width, this.Height);
+            gra = Graphics.FromImage(bm);
+            pen = new Pen(Color.Black, currentSize);
+
+            //Dieu chinh net ve cho but
+            pen.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
+
+            //Giam hien tuong rung khi ve
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+
+        }
+
+        
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog cld = new ColorDialog();
+            if(cld.ShowDialog() == DialogResult.OK)
+            {
+                ColorButton.BackColor = cld.Color;
+                currentColor = cld.Color;
+                pen.Color = cld.Color;
+            }
+        }
+
+        private void InitComboBox()
+        {
+            for(int i=2; i<=10; i+=2)
+            {
+                comboBox1.Items.Add(i);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pen = new Pen(currentColor, Convert.ToInt32(comboBox1.Text));
+            pen.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDown = false;
+            Pen pen = new Pen(currentColor, currentSize);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDown = true;
+            old = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDown)
+            {
+                current = e.Location;
+                gra.DrawLine(pen, old, current);
+                this.BackgroundImage = (Bitmap)bm.Clone();
+                old = current;
+            }
+        }
+    }
+}
