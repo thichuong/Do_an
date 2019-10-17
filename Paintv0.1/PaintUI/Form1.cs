f538a06aa405bcad5247a09ad6e4d2f31413b2e4
atbash111 <chuongtangkhanh2104@gmail.com>
2019-10-17 22:23:33 +0700

    Update 17-10

---
+++ Paintv0.1/PaintUI/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintUI
+{
+    public partial class Form1 : Form
+    {
+        //Khai bao bien
+        Pen pen;
+        Bitmap bm;
+        Graphics gra;
+        Point old, cur;
+        bool isDown;
+        int wid, hei;
+
+        public Form1()
+        {
+            InitializeComponent();
+            HideAllPanel();
+            brushesPanel.Show();
+            pen = new Pen(Color.Black, 10);
+            bm = new Bitmap(SketchBox.Width, SketchBox.Height, SketchBox.CreateGraphics());
+            gra = Graphics.FromImage(bm);
+            isDown = false;
+
+            //Modify stroke
+            pen.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
+
+
+            //Smoothing
+            {
+                this.SetStyle(ControlStyles.UserPaint, true);
+                this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+                this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+                gra.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                gra.Clear(Color.White);
+            }
+
+        }
+        //Giau Panels
+        private void HideAllPanel()
+        {
+            shapesPanel.Visible = false;
+            textPanel.Visible = false;
+            canvasPanel.Visible = false;
+            brushesPanel.Visible = false;
+            effectsPanel.Visible = false;
+        }
+        
+        
+        
+        //Code cac chuc nang cho cac WindowState Butttons
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void MaximizeButton_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState != FormWindowState.Maximized)
+                this.WindowState = FormWindowState.Maximized;
+            else this.WindowState = FormWindowState.Normal;
+        }
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        
+        //Hien thi cac Panels khi click
+        private void TextButton_Click(object sender, EventArgs e)
+        {
+            if (!textPanel.Visible)
+            {
+                HideAllPanel();
+                BunifuTransition1.ShowSync(textPanel, false, BunifuAnimatorNS.Animation.HorizSlide);
+            }
+            
+        }
+        private void ShapesButton_Click(object sender, EventArgs e)
+        {
+            if (!shapesPanel.Visible)
+            {
+                HideAllPanel();
+                BunifuTransition1.ShowSync(shapesPanel, false, BunifuAnimatorNS.Animation.HorizSlide);
+            }
+                 
+        }
+        private void CanvasButton_Click(object sender, EventArgs e)
+        {
+            if (!canvasPanel.Visible)
+            {
+                HideAllPanel();
+                BunifuTransition1.ShowSync(canvasPanel, false, BunifuAnimatorNS.Animation.HorizSlide);
+            }
+          
+        }
+
+        private void BrushesButton_Click(object sender, EventArgs e)
+        {
+            if (!brushesPanel.Visible)
+            {
+                HideAllPanel();
+                BunifuTransition1.ShowSync(brushesPanel, false, BunifuAnimatorNS.Animation.HorizSlide);
+            }
+        }
+        private void EffectsButton_Click(object sender, EventArgs e)
+        {
+            if (!effectsPanel.Visible)
+            {
+                HideAllPanel();
+                BunifuTransition1.ShowSync(effectsPanel, false, BunifuAnimatorNS.Animation.HorizSlide);
+            }
+                
+        }
+
+        //Cac su kien voi mouse
+        private void SketchBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDown)
+            {
+                cur = new Point(e.Location.X, e.Location.Y);
+                wid = cur.X - old.X;
+                hei = cur.Y - old.Y;
+                SketchBox.Refresh();
+
+            }
+        }
+
+        private void SketchBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDown = false;
+            wid = hei = 0;
+        }
+
+        private void MenuButton_Click(object sender, EventArgs e)
+        {
+            menuPanel1.BringToFront();
+        }
+
+        private void SketchBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDown = true;
+            old = new Point(e.Location.X, e.Location.Y);
+        }
+
+        private void SketchBox_Paint(object sender, PaintEventArgs e)
+        {
+            if(isDown)
+            {
+                gra.DrawLine(pen, old, cur);
+                old = cur;
+                SketchBox.BackgroundImage = (Bitmap)bm.Clone();
+            }
+        }
+
+        //---------------
+
+    }
+}
---
+++ Paintv0.1/PaintUI/TextPanel.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PaintUI
+{
+    partial class TextPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Text_SizeComboBox = new System.Windows.Forms.ComboBox();
+            this.Text_FontCombobox = new System.Windows.Forms.ComboBox();
+            this.Text_Label2 = new System.Windows.Forms.Label();
+            this.Text_Label1 = new System.Windows.Forms.Label();
+            this.Text_CreateTextBoxButton = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.Text_Label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Text_SizeComboBox
+            // 
+            this.Text_SizeComboBox.Font = new System.Drawing.Font("Segoe Print", 12F);
+            this.Text_SizeComboBox.FormattingEnabled = true;
+            this.Text_SizeComboBox.Location = new System.Drawing.Point(34, 388);
+            this.Text_SizeComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Text_SizeComboBox.Name = "Text_SizeComboBox";
+            this.Text_SizeComboBox.Size = new System.Drawing.Size(258, 50);
+            this.Text_SizeComboBox.TabIndex = 9;
+            // 
+            // Text_FontCombobox
+            // 
+            this.Text_FontCombobox.Font = new System.Drawing.Font("Segoe Print", 12F);
+            this.Text_FontCombobox.FormattingEnabled = true;
+            this.Text_FontCombobox.Location = new System.Drawing.Point(34, 258);
+            this.Text_FontCombobox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Text_FontCombobox.Name = "Text_FontCombobox";
+            this.Text_FontCombobox.Size = new System.Drawing.Size(258, 50);
+            this.Text_FontCombobox.TabIndex = 8;
+            // 
+            // Text_Label2
+            // 
+            this.Text_Label2.AutoSize = true;
+            this.Text_Label2.BackColor = System.Drawing.Color.Transparent;
+            this.Text_Label2.Font = new System.Drawing.Font("Segoe Print", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Text_Label2.Location = new System.Drawing.Point(34, 323);
+            this.Text_Label2.Name = "Text_Label2";
+            this.Text_Label2.Size = new System.Drawing.Size(82, 52);
+            this.Text_Label2.TabIndex = 7;
+            this.Text_Label2.Text = "Size";
+            // 
+            // Text_Label1
+            // 
+            this.Text_Label1.AutoSize = true;
+            this.Text_Label1.BackColor = System.Drawing.Color.Transparent;
+            this.Text_Label1.Font = new System.Drawing.Font("Segoe Print", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Text_Label1.Location = new System.Drawing.Point(34, 186);
+            this.Text_Label1.Name = "Text_Label1";
+            this.Text_Label1.Size = new System.Drawing.Size(92, 52);
+            this.Text_Label1.TabIndex = 5;
+            this.Text_Label1.Text = "Font";
+            // 
+            // Text_CreateTextBoxButton
+            // 
+            this.Text_CreateTextBoxButton.Activecolor = System.Drawing.Color.Transparent;
+            this.Text_CreateTextBoxButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.Text_CreateTextBoxButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Text_CreateTextBoxButton.BorderRadius = 0;
+            this.Text_CreateTextBoxButton.ButtonText = "Create TextBox        ";
+            this.Text_CreateTextBoxButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Text_CreateTextBoxButton.DisabledColor = System.Drawing.Color.Gray;
+            this.Text_CreateTextBoxButton.Iconcolor = System.Drawing.Color.Transparent;
+            this.Text_CreateTextBoxButton.Iconimage = global::PaintUI.Properties.Resources.create;
+            this.Text_CreateTextBoxButton.Iconimage_right = null;
+            this.Text_CreateTextBoxButton.Iconimage_right_Selected = null;
+            this.Text_CreateTextBoxButton.Iconimage_Selected = null;
+            this.Text_CreateTextBoxButton.IconMarginLeft = 0;
+            this.Text_CreateTextBoxButton.IconMarginRight = 0;
+            this.Text_CreateTextBoxButton.IconRightVisible = true;
+            this.Text_CreateTextBoxButton.IconRightZoom = 0D;
+            this.Text_CreateTextBoxButton.IconVisible = true;
+            this.Text_CreateTextBoxButton.IconZoom = 70D;
+            this.Text_CreateTextBoxButton.IsTab = false;
+            this.Text_CreateTextBoxButton.Location = new System.Drawing.Point(43, 104);
+            this.Text_CreateTextBoxButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Text_CreateTextBoxButton.Name = "Text_CreateTextBoxButton";
+            this.Text_CreateTextBoxButton.Normalcolor = System.Drawing.Color.Gainsboro;
+            this.Text_CreateTextBoxButton.OnHovercolor = System.Drawing.Color.Silver;
+            this.Text_CreateTextBoxButton.OnHoverTextColor = System.Drawing.Color.Black;
+            this.Text_CreateTextBoxButton.selected = false;
+            this.Text_CreateTextBoxButton.Size = new System.Drawing.Size(259, 62);
+            this.Text_CreateTextBoxButton.TabIndex = 6;
+            this.Text_CreateTextBoxButton.Text = "Create TextBox        ";
+            this.Text_CreateTextBoxButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Text_CreateTextBoxButton.Textcolor = System.Drawing.Color.Black;
+            this.Text_CreateTextBoxButton.TextFont = new System.Drawing.Font("Segoe Print", 10F);
+            // 
+            // Text_Label3
+            // 
+            this.Text_Label3.AutoSize = true;
+            this.Text_Label3.BackColor = System.Drawing.Color.Transparent;
+            this.Text_Label3.Font = new System.Drawing.Font("Segoe Print", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Text_Label3.Location = new System.Drawing.Point(102, 22);
+            this.Text_Label3.Name = "Text_Label3";
+            this.Text_Label3.Size = new System.Drawing.Size(123, 52);
+            this.Text_Label3.TabIndex = 5;
+            this.Text_Label3.Text = "Text 2";
+            // 
+            // TextPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Controls.Add(this.Text_SizeComboBox);
+            this.Controls.Add(this.Text_FontCombobox);
+            this.Controls.Add(this.Text_Label2);
+            this.Controls.Add(this.Text_CreateTextBoxButton);
+            this.Controls.Add(this.Text_Label3);
+            this.Controls.Add(this.Text_Label1);
+            this.Name = "TextPanel";
+            this.Size = new System.Drawing.Size(334, 515);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Text_SizeComboBox;
+        private System.Windows.Forms.ComboBox Text_FontCombobox;
+        private System.Windows.Forms.Label Text_Label2;
+        private Bunifu.Framework.UI.BunifuFlatButton Text_CreateTextBoxButton;
+        private System.Windows.Forms.Label Text_Label1;
+        private System.Windows.Forms.Label Text_Label3;
+    }
+}
