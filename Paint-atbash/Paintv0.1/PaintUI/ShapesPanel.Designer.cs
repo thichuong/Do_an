2070f93a77d3d3063f16b453a80fc9d94e1e1904
atbash111 <chuongtangkhanh2104@gmail.com>
2019-10-18 11:01:51 +0700

    Update 18-10

---
+++ Paint-atbash/Paintv0.1/PaintUI/ShapesPanel.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PaintUI
+{
+    partial class ShapesPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Shapes_OutlineCheckBox = new Bunifu.Framework.UI.BunifuCheckbox();
+            this.Shapes_Label4 = new System.Windows.Forms.Label();
+            this.Shapes_FillCheckBox = new Bunifu.Framework.UI.BunifuCheckbox();
+            this.Shapes_Label3 = new System.Windows.Forms.Label();
+            this.Shapes_SizeComboBox = new System.Windows.Forms.ComboBox();
+            this.Shapes_ShapeComboBox = new System.Windows.Forms.ComboBox();
+            this.Shapes_Label2 = new System.Windows.Forms.Label();
+            this.Shapes_Label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Shapes_OutlineCheckBox
+            // 
+            this.Shapes_OutlineCheckBox.BackColor = System.Drawing.Color.DarkCyan;
+            this.Shapes_OutlineCheckBox.ChechedOffColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(135)))), ((int)(((byte)(140)))));
+            this.Shapes_OutlineCheckBox.Checked = true;
+            this.Shapes_OutlineCheckBox.CheckedOnColor = System.Drawing.Color.DarkCyan;
+            this.Shapes_OutlineCheckBox.ForeColor = System.Drawing.Color.White;
+            this.Shapes_OutlineCheckBox.Location = new System.Drawing.Point(259, 478);
+            this.Shapes_OutlineCheckBox.Margin = new System.Windows.Forms.Padding(9, 14, 9, 14);
+            this.Shapes_OutlineCheckBox.Name = "Shapes_OutlineCheckBox";
+            this.Shapes_OutlineCheckBox.Size = new System.Drawing.Size(20, 20);
+            this.Shapes_OutlineCheckBox.TabIndex = 16;
+            // 
+            // Shapes_Label4
+            // 
+            this.Shapes_Label4.AutoSize = true;
+            this.Shapes_Label4.BackColor = System.Drawing.Color.Transparent;
+            this.Shapes_Label4.Font = new System.Drawing.Font("Segoe Print", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Shapes_Label4.Location = new System.Drawing.Point(23, 459);
+            this.Shapes_Label4.Name = "Shapes_Label4";
+            this.Shapes_Label4.Size = new System.Drawing.Size(133, 52);
+            this.Shapes_Label4.TabIndex = 15;
+            this.Shapes_Label4.Text = "Outline";
+            // 
+            // Shapes_FillCheckBox
+            // 
+            this.Shapes_FillCheckBox.BackColor = System.Drawing.Color.DarkCyan;
+            this.Shapes_FillCheckBox.ChechedOffColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(135)))), ((int)(((byte)(140)))));
+            this.Shapes_FillCheckBox.Checked = true;
+            this.Shapes_FillCheckBox.CheckedOnColor = System.Drawing.Color.DarkCyan;
+            this.Shapes_FillCheckBox.ForeColor = System.Drawing.Color.White;
+            this.Shapes_FillCheckBox.Location = new System.Drawing.Point(259, 378);
+            this.Shapes_FillCheckBox.Margin = new System.Windows.Forms.Padding(9, 14, 9, 14);
+            this.Shapes_FillCheckBox.Name = "Shapes_FillCheckBox";
+            this.Shapes_FillCheckBox.Size = new System.Drawing.Size(20, 20);
+            this.Shapes_FillCheckBox.TabIndex = 14;
+            // 
+            // Shapes_Label3
+            // 
+            this.Shapes_Label3.AutoSize = true;
+            this.Shapes_Label3.BackColor = System.Drawing.Color.Transparent;
+            this.Shapes_Label3.Font = new System.Drawing.Font("Segoe Print", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Shapes_Label3.Location = new System.Drawing.Point(23, 359);
+            this.Shapes_Label3.Name = "Shapes_Label3";
+            this.Shapes_Label3.Size = new System.Drawing.Size(68, 52);
+            this.Shapes_Label3.TabIndex = 13;
+            this.Shapes_Label3.Text = "Fill";
+            // 
+            // Shapes_SizeComboBox
+            // 
+            this.Shapes_SizeComboBox.Font = new System.Drawing.Font("Segoe Print", 12F);
+            this.Shapes_SizeComboBox.FormattingEnabled = true;
+            this.Shapes_SizeComboBox.Location = new System.Drawing.Point(23, 272);
+            this.Shapes_SizeComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Shapes_SizeComboBox.Name = "Shapes_SizeComboBox";
+            this.Shapes_SizeComboBox.Size = new System.Drawing.Size(258, 50);
+            this.Shapes_SizeComboBox.TabIndex = 12;
+            // 
+            // Shapes_ShapeComboBox
+            // 
+            this.Shapes_ShapeComboBox.Font = new System.Drawing.Font("Segoe Print", 12F);
+            this.Shapes_ShapeComboBox.FormattingEnabled = true;
+            this.Shapes_ShapeComboBox.Location = new System.Drawing.Point(23, 109);
+            this.Shapes_ShapeComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Shapes_ShapeComboBox.Name = "Shapes_ShapeComboBox";
+            this.Shapes_ShapeComboBox.Size = new System.Drawing.Size(258, 50);
+            this.Shapes_ShapeComboBox.TabIndex = 11;
+            // 
+            // Shapes_Label2
+            // 
+            this.Shapes_Label2.AutoSize = true;
+            this.Shapes_Label2.BackColor = System.Drawing.Color.Transparent;
+            this.Shapes_Label2.Font = new System.Drawing.Font("Segoe Print", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Shapes_Label2.Location = new System.Drawing.Point(23, 196);
+            this.Shapes_Label2.Name = "Shapes_Label2";
+            this.Shapes_Label2.Size = new System.Drawing.Size(82, 52);
+            this.Shapes_Label2.TabIndex = 10;
+            this.Shapes_Label2.Text = "Size";
+            // 
+            // Shapes_Label1
+            // 
+            this.Shapes_Label1.AutoSize = true;
+            this.Shapes_Label1.BackColor = System.Drawing.Color.Transparent;
+            this.Shapes_Label1.Font = new System.Drawing.Font("Segoe Print", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Shapes_Label1.Location = new System.Drawing.Point(23, 34);
+            this.Shapes_Label1.Name = "Shapes_Label1";
+            this.Shapes_Label1.Size = new System.Drawing.Size(127, 52);
+            this.Shapes_Label1.TabIndex = 9;
+            this.Shapes_Label1.Text = "Shapes";
+            // 
+            // ShapesPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Shapes_OutlineCheckBox);
+            this.Controls.Add(this.Shapes_Label4);
+            this.Controls.Add(this.Shapes_FillCheckBox);
+            this.Controls.Add(this.Shapes_Label3);
+            this.Controls.Add(this.Shapes_SizeComboBox);
+            this.Controls.Add(this.Shapes_ShapeComboBox);
+            this.Controls.Add(this.Shapes_Label2);
+            this.Controls.Add(this.Shapes_Label1);
+            this.Name = "ShapesPanel";
+            this.Size = new System.Drawing.Size(331, 563);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuCheckbox Shapes_OutlineCheckBox;
+        private System.Windows.Forms.Label Shapes_Label4;
+        private Bunifu.Framework.UI.BunifuCheckbox Shapes_FillCheckBox;
+        private System.Windows.Forms.Label Shapes_Label3;
+        private System.Windows.Forms.ComboBox Shapes_SizeComboBox;
+        private System.Windows.Forms.ComboBox Shapes_ShapeComboBox;
+        private System.Windows.Forms.Label Shapes_Label2;
+        private System.Windows.Forms.Label Shapes_Label1;
+    }
+}
