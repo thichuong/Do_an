e77e6d757755eefcd494b798c61e53fb6d238623
DuCheAnh <49919557+DuCheAnh@users.noreply.github.com>
2019-12-09 02:15:30 +0700

    Add files via upload

---
+++ Paintv0.1/PaintUI/Ribbon.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintUI
+{
+    public partial class Ribbon : UserControl
+    {
+        Form1 parent;
+        public Ribbon()
+        {
+            InitializeComponent();
+            parent = (Form1)this.ParentForm;
+        }
+
+        private void TextButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ShapesButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CanvasButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BrushesButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EffectsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowPanel(BrushesPanel tar)
+        {
+
+        }
+
+        private void ShowPanel(EffectsPanel tar)
+        {
+
+        }
+
+        //private void ShowPanel(ShapesPanel tar)
+        //{
+        //    if (!tar.Visible)
+        //    {
+        //        HideAllPanel();
+        //        tar.Show();
+        //    }
+        //    curTool = Tools.SHAPE;
+        //}
+
+        //private void ShowPanel(CanvasPanel tar)
+        //{
+
+        //}
+
+        //private void ShowPanel(TextPanel tar)
+        //{
+        //    if (!tar.Visible)
+        //    {
+        //        HideAllPanel();
+        //        tar.Show();
+        //    }
+        //}
+    }
+}
